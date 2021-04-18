e11c60169842e0d676e17fd65fff3fde8bc3e714
Mason Nakamura <Mason.Nakamura1@marist.edu>
2021-04-18 17:28:01 -0400

    Assignment 6 done

---
+++ Assignments/Assignment 6/src/base/Main.java
@@ -0,0 +1,11 @@
+package src.base;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Plant flower = new Plant(true, "Plant Family 1", "Flower1");
+        flower.wateringInstructions();
+        flower.sunlightRequirements();
+
+    }
+}
